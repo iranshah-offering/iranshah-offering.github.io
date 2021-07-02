17b5bacf992fd9753a6c38929bd8852b7b164346
iranshah-offering <86184678+iranshah-offering@users.noreply.github.com>
2021-07-02 23:49:09 +0530

    Update ExtraFields.js

--- ExtraFields.js
+++ ExtraFields.js
@@ -8,7 +8,7 @@ ec.order.extraFields.full_name = {
     'type': 'text',
     'required': true,
     'checkoutDisplaySection': 'payment_details', // show new field in order comments block
-    'orderDetailsDisplaySection': 'order_comments' // show saved data in order comments block in order details to merchant and customer
+    'orderDetailsDisplaySection': 'customer_info' // show saved data in order comments block in order details to merchant and customer
 };
 
 // Add new text field to order comments section at checkout
@@ -17,7 +17,7 @@ ec.order.extraFields.phone_number = {
     'type': 'text',
     'required': true,
     'checkoutDisplaySection': 'payment_details', // show new field in order comments block
-    'orderDetailsDisplaySection': 'order_comments' // show saved data in order comments block in order details to merchant and customer
+    'orderDetailsDisplaySection': 'customer_info' // show saved data in order comments block in order details to merchant and customer
 };
 
 // Add new text field to order comments section at checkout
